fe80a47dbae9e02b74b102fe71b5e60b792e985f
Tobias Alves <102394909+TobiasAlvz@users.noreply.github.com>
2026-05-08 16:49:07 -0300

    Update router.js

--- router.js
+++ router.js
@@ -1,9 +1,31 @@
-const express = require ('express');
+const express = require('express');
 
-const router = express.Router ();
+const router = express.Router();
 
-router.get ('/', (req, res) => {
-  res.send ('hello, world');
-});
+// Controllers
+const postController = require('./src/controllers/postController');
+const adminController = require('./src/controllers/adminController');
+
+// Página inicial
+router.get('/', postController.index);
+
+// Página do post
+router.get('/posts/:id', postController.show);
+
+// ================= ADMIN =================
+
+// Página admin
+router.get('/admin', adminController.showAdminPage);
+
+// Criar post
+router.get('/admin/create', adminController.showCreatePage);
+router.post('/admin/create', adminController.createPost);
+
+// Editar post
+router.get('/admin/edit/:id', adminController.showEditPage);
+router.post('/admin/update/:id', adminController.updatePost);
+
+// Deletar post
+router.post('/admin/delete/:id', adminController.deletePost);
 
 module.exports = router;
